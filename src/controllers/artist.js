09d349a5464d247f74757b0071c7c68feb61f04e
Rob B <rob@rjrobbie.dev>
2023-03-08 20:26:36 +0000

    Create test find out the details of a single artist

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,5 +1,15 @@
 const db = require('../db/index.js')
 
+const readArtist = async (req, res) => {
+
+    try {
+      const result = await db.query('SELECT * FROM Artists');
+      res.status(200).json(result.rows)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+}
+
 const createArtist = async (req, res) => {
     const { name, genre } = req.body
   
@@ -11,6 +21,26 @@ const createArtist = async (req, res) => {
     }
   }
 
+  const getArtistById = async (req, res) => {
+    try {
+      const { id } = req.params
+      const { rows: [ artist ] } = await db.query('SELECT * FROM Artists WHERE id = $1', [ id ])
+  
+      if (!artist) {
+        return res.status(404).json({ message: `artist ${id} does not exist` })
+      }
+  
+      res.status(200).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+
+
+
 module.exports = {
-    createArtist
+    readArtist,
+    createArtist,
+    getArtistById
 }
+
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { createArtist } = require('../controllers/artist.js')
+const artistController = require('../controllers/artist.js')
 
 router.post('/', createArtist)
+
+router.get('/', artistController.readArtist)
+
+router.get('/:id', artistController.getArtistById);
+
 
 module.exports = router;
